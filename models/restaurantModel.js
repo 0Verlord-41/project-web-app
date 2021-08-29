d3b544a8cbc0d1b43d5f338010b43860fce930d5
0Verlord-41 <kinicooo1@gmail.com>
2021-08-29 22:51:15 +0530

    feat: update
    - updated restaurant model

--- models/restaurantModel.js
+++ models/restaurantModel.js
@@ -1,48 +1,56 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const restaurantSchema = new mongoose.Schema({
-    //schema definition object
-    name: {
-        type: String,
-        required: [true, 'A Restaurant must have a name'],
-        unique: true,
-        trim: true,
-        maxlength: [40, 'A Restaurant name must have less than or equal to 40 char'],
-        minlength: [5, 'A Restaurant name must have more than or equal to 5 char']
-    },
-    ratingsAverage: {
-        type: Number,
-        default: 4.5,
-        min: [1, 'Rating must be above 1'],
-        max: [5, 'Rating must be below 5']
-    },
-    ratingsQuantity: {
-        type: Number,
-        default: 0
-    },
-    description: {
-        type: String,
-        trim: true,
-        required: [true, 'A Restaurant must have a description']
-    },
-    imageCover: {
-        type: String,
-        required: [true, 'A tour must have a cover image']
-    },
-    images: [String],
-    createdAt:{
-        type: Date,
-        default: Date.now(),
-        select: false
-    }
+const restschema=new mongoose.Schema({
+  name:{
+    type:String
+  },
+  Locations:[{
+   
+    coordinates:[Number]
+  }],
+  Type:{
+  type:String
+  },
+  price:{
+    type:String
+  },
+  Description:{
+    type:String
+  },
+  ratings:{
+    type:Number
+  },
+  averagerating:{
+    type:String
+  },
+  Cooks:{
+    type:Number
+  },
+  summary:{
+    type:String
+  },
+  Place:{
+    type:String
+  },
+  images:{
+    type:String
+  },
+  Chef:{
+    type:String
+  },
+  chefphoto:[String],
+  Contact:{
+    type:String
+  },
+  moreimg:[String]
+
+},
+{
+    toJSON:{virtuals:true},
+    toObjects:{virtuals:true}
 });
 
-tourSchema.pre('save', function(next){
-    console.log('Will save document...');
-    next();
-});
 
-const Restaurant = mongoose.model('Restaurant', restaurantSchema);
-
-module.exports = Restaurant;
+const Rest=mongoose.model('Rest',restschema);
+module.exports=Rest;
