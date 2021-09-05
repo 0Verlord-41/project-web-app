3a39940934fdbd9872d267f46f0584a957231ba1
0Verlord-41 <kinicooo1@gmail.com>
2021-09-05 19:22:35 +0530

    feat: update
    - JSON file for restaurant data
    - Mongoose added to connect to Atlas DB

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env'});
 
 const app = require('./app');
 
-// const DB = process.env.DATABASE.replace(
-//     '<PASSWORD>',
-//     process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
 
-// mongoose.connect(DB, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false
-// }).then(() => console.log('DB Connection Successful!'))
+mongoose.connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('DB Connection Successful!'))
 
 const port = process.env.PORT || 8000;
 console.log(app.get('env'));
