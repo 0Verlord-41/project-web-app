a2bf320ff4314ea7a378e8db4d90f6b0550c3beb
0Verlord-41 <kinicooo1@gmail.com>
2021-12-03 22:43:51 +0530

    feat: update
    - Packages update
    - server update

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+  });
+
 dotenv.config({ path: './config.env'});
 
 const app = require('./app');
@@ -20,6 +26,21 @@ mongoose.connect(DB, {
 const port = process.env.PORT || 8000;
 console.log(app.get('env'));
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Working on port: ${port}...`);
 });
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+
+  process.on('SIGTERM', () => {
+    console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+    server.close(() => {
+      console.log('💥 Process terminated!');
+    });
+  });
